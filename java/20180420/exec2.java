92705b5a1d144b2aabeaeb64b9624c54c079bcae
wjsaya@centos7 <sayawj@163.com>
2018-04-21 00:45:02 +0800

    deleted:    java/20180420/calu.class 	deleted:    java/20180420/ex2.class 	deleted:    java/20180420/exec1.class 	deleted:    java/20180420/exec2.class 	modified:   java/20180420/exec2.java

--- java/20180420/exec2.java
+++ java/20180420/exec2.java
@@ -53,18 +53,48 @@ class ex2 {
         return result; 
     }
 
+    ex2 pwn(ex2 num1) {
+    //(a+bi)(c+di)=(ac-bd)+(bc+ad)i.
+        double s2 = num1.getReal();
+        double x2 = num1.getVirtual();
+        double news = (s*s2 - x*x2);
+        double newx = (s2*x + s*x2);
+        ex2 result = new ex2();
+        result.set(news, newx);
+        return result;
+    }
+
+    ex2 mod(ex2 num1) {
+    //(a+bi)/(c+di)=(ac+bd)/(c^2+d^2) +((bc-ad)/(c^2+d^2))i
+        double s2 = num1.getReal();
+        double x2 = num1.getVirtual();
+        double news = (s*s2+x*x2)/(s2*s2 + x2*x2);
+        double newx = (x*s2-s*x2)/(s2*s2 + x2*x2);
+        ex2 result = new ex2();
+        result.set(news, newx);
+        return result;
+    }
+
 }
 
 public class exec2 {
     public static void main(String[] args) {
     ex2 num1 = new ex2();
     ex2 num2 = new ex2();
-    num2.set(3, 3);
-    num1.set(1, 1);
-    System.out.println("num1--"+num1.getReal()+"---"+num1.getVirtual());
-    System.out.println("num2--"+num2.getReal()+"---"+num2.getVirtual());
-    ex2 nnn = num1.add(num2);
-    System.out.println("new--"+nnn.getReal()+"---"+nnn.getVirtual());
+    num1.set(1, 2);
+    num2.set(3, 4);
+    
+    ex2 new1 = num1.add(num2);
+    ex2 new2 = num1.sub(num2);
+    ex2 new3 = num1.pwn(num2);
+    ex2 new4 = num1.mod(num2);
+
+    System.out.println("num1:\t"+num1.getReal()+" + i*"+num1.getVirtual());
+    System.out.println("num2:\t"+num2.getReal()+" + i*"+num2.getVirtual());
+    System.out.println("+:\t"+new1.getReal()+" + i*"+new1.getVirtual());
+    System.out.println("-:\t"+new2.getReal()+" + i*"+new2.getVirtual());
+    System.out.println("*:\t"+new3.getReal()+" + i*"+new3.getVirtual());
+    System.out.println("/:\t"+new4.getReal()+" + i*"+new4.getVirtual());
     }
 
 }
