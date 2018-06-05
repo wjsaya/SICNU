40942133b1a9f1a30682e9251d5b90d025cef6c5
wjsaya@centos7 <sayawj@163.com>
2018-06-05 15:31:07 +0800

    modified:   C数据结构与算法/TODO/6-13_二叉搜索树的操作集.c

--- C数据结构与算法/TODO/6-13_二叉搜索树的操作集.c
+++ C数据结构与算法/TODO/6-13_二叉搜索树的操作集.c
@@ -9,7 +9,6 @@ struct TNode{
     BinTree Left;
     BinTree Right;
 };
-
 void PreorderTraversal( BinTree BT ); /* 先序遍历，由裁判实现，细节不表 */
 void InorderTraversal( BinTree BT );  /* 中序遍历，由裁判实现，细节不表 */
 
@@ -51,10 +50,116 @@ int main()
         BST = Delete(BST, X);
     }
     printf("Inorder:"); InorderTraversal(BST); printf("\n");
-
     return 0;
 }
 /* 你的代码将被嵌在这里 */
+
+BinTree Insert( BinTree BST, ElementType X )
+{
+//    函数Insert将X插入二叉搜索树BST并返回结果树的根结点指针；
+//插入当前，左，右
+    if (BST == NULL) {
+        BST = (struct TNode *)malloc(sizeof(struct TNode));
+        BST->Data = X;
+        BST->Left = NULL;
+        BST->Right = NULL;
+        return BST;
+    }
+    if(X < BST->Data) {
+        BST->Left = Insert(BST->Left, X);
+    }
+    if(X > BST->Data) {
+        BST->Right = Insert(BST->Right, X);
+    }
+    return BST;
+}
+
+BinTree Delete( BinTree BST, ElementType X )
+{
+//    函数Delete将X从二叉搜索树BST中删除，并返回结果树的根结点指针；如果X不在树中，则打印一行Not Found并返回原树的根结点指针；
+    Position Tmp;
+//    if (Find(BST, X) == NULL) {
+    if(!BST) {
+        printf("Not Found\n");
+    }
+    else {
+        if(X < BST->Data)
+            BST->Left = Delete(BST->Left, X);
+        else if(X > BST->Data)
+            BST->Right = Delete(BST->Right, X);
+        else {
+            if(BST->Left && BST->Right) {
+            //有左右节点
+            Tmp = FindMin(BST->Right);
+            BST->Data = Tmp->Data;
+            BST->Right = Delete(BST->Right, BST->Data);
+            }
+            else{
+                Tmp = BST;
+                if(BST->Left == NULL)
+                    BST = BST->Right;
+                else if(BST->Right == NULL)
+                    BST = BST->Left;
+                free(Tmp);
+            }
+        }
+    }
+}
+
+Position Find( BinTree BST, ElementType X )
+{
+//    函数Find在二叉搜索树BST中找到X，返回该结点的指针；如果找不到则返回空指针；
+    if (BST == NULL)
+        return BST;
+    if(X == BST->Data)
+        return BST;
+    if(X > BST->Data)
+        return Find(BST->Right, X);
+    if(X < BST->Data)
+        return Find(BST->Left, X);
+}
+
+Position FindMin( BinTree BST )
+{
+//    函数FindMin返回二叉搜索树BST中最小元结点的指针；
+    if(BST) {
+        while (BST->Left != NULL)
+            BST = BST->Left;
+    }
+    return BST;
+}
+
+Position FindMax( BinTree BST )
+{
+//    函数FindMax返回二叉搜索树BST中最大元结点的指针。
+    if(BST) {
+        while(BST->Right != NULL)
+            BST = BST->Right;
+    }
+    return BST;
+}
+
+
+void PreorderTraversal( BinTree BT )
+{
+/* 先序遍历，由裁判实现，细节不表 */
+    if(BT)
+    {
+        printf(" %d", BT->Data);
+        PreorderTraversal(BT->Left);
+        PreorderTraversal(BT->Right);
+    }
+}
+void InorderTraversal( BinTree BT )
+{
+  /* 中序遍历，由裁判实现，细节不表 */
+    if(BT)
+    {
+        InorderTraversal(BT->Left);
+        printf(" %d", BT->Data);
+        InorderTraversal(BT->Right);
+    }
+}
 
 
 
