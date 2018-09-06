8dce80153e772fa9d45b81aaa218313c2001413f
wjsaya <sayawj@163.com>
2018-09-06 12:45:11 +0800

    modified:   algorithms/binarySearch.c

--- algorithms/binarySearch.c
+++ algorithms/binarySearch.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 // 二分查找
 int erfen(int left, int right, int list[]) {
-
-
+    printf("%d\n%d\n", left, right);
+    
+    return 0;
 }
 
 
 int main(void) {
     int li[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-
-return 0;
+    erfen(0, 9, li);
+    return 0;
 }
