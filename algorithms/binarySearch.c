bc1a0f89cf4c273057e657a401a5babe78d226c8
wjsaya@centos7 <sayawj@163.com>
2018-09-06 13:05:31 +0800

    binarySearch done

--- algorithms/binarySearch.c
+++ algorithms/binarySearch.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
-// 二分查找
-int erfen(int left, int right, int list[]) {
-    printf("%d\n%d\n", left, right);
+// binary Search
+int erfen(int target, int left, int right, int list[]) {
+    if (left <= right)  
+        return -1;
+    // when left<=right, means don't find target
+    // so Just return.
+
+    int half = (left + right)/2;
+    // Between list[5:9],the middle is 7, Equals(5+9)/2
     
-    return 0;
+    if(target == list[half]) 
+        return half;
+        // find it. return the position.
+
+    else if(target < list[half]) 
+        return erfen(target, left, half-1, list);
+        // smaller? find it on left side.
+
+    else    
+        return erfen(target, half+1, right, list);
+        // bigger? find it on right side.
 }
 
 
 int main(void) {
-    int li[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    erfen(0, 9, li);
+    int li[10] = {2, 6, 7, 9, 14, 25, 36, 47, 58, 69};
+    int re, tar;
+    printf("input a Integer to find:");
+    scanf("%d", &tar);
+    re = erfen(tar, 0, 9, li);
+    printf("%d\n", re);
     return 0;
 }
